f77e6012fb2f023a1e0b1dcda511f172d8e6c069
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-06-18 12:51:40 +0530

    made admin list hidden in menu and responded 404 at route /admins for seller admins

--- packages/evershop/src/modules/admin/pages/admin/adminGrid/index.js
+++ packages/evershop/src/modules/admin/pages/admin/adminGrid/index.js
@@ -7,6 +7,11 @@ const {
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (request, response) => {
+  const isSuperAdmin = request.isSuperAdmin();
+  if (!isSuperAdmin) {
+    response.status(404);
+    return;
+  }
   setContextValue(request, 'pageInfo', {
     title: 'Admins',
     description: 'Admins'
--- packages/evershop/src/modules/admin/pages/admin/all/AdminMenuGroup.jsx
+++ packages/evershop/src/modules/admin/pages/admin/all/AdminMenuGroup.jsx
@@ -3,24 +3,31 @@ import React from 'react';
 import UsersIcon from '@heroicons/react/solid/esm/UsersIcon';
 import NavigationItemGroup from '@components/admin/cms/NavigationItemGroup';
 
-export default function AdminMenuGroup({ adminGrid }) {
+export default function AdminMenuGroup({ adminGrid, isSuperAdmin }) {
+  const items = [];
+  if (isSuperAdmin) {
+    items.push({
+      Icon: UsersIcon,
+      url: adminGrid,
+      title: 'Admin Users'
+    });
+  }
+
+  if (items.length === 0) {
+    return null;
+  }
   return (
     <NavigationItemGroup
       id="adminMenuGroup"
       name="Admin"
-      items={[
-        {
-          Icon: UsersIcon,
-          url: adminGrid,
-          title: 'Admins'
-        }
-      ]}
+      items={items}
     />
   );
 }
 
 AdminMenuGroup.propTypes = {
-  adminGrid: PropTypes.string.isRequired
+  adminGrid: PropTypes.string.isRequired,
+  isSuperAdmin: PropTypes.bool.isRequired
 };
 
 export const layout = {
@@ -31,5 +38,6 @@ export const layout = {
 export const query = `
   query Query {
     adminGrid: url(routeId:"adminGrid")
+    isSuperAdmin: isCurrentAdminUserSuperAdmin
   }
 `;
