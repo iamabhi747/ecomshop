9ca0bb045a7988e1aab73b71fa0b1c424c5f719e
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-06-18 17:19:12 +0530

    added actions to adminGrid to update admins

--- packages/evershop/src/modules/admin/pages/admin/adminGrid/Grid.jsx
+++ packages/evershop/src/modules/admin/pages/admin/adminGrid/Grid.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-nested-ternary */
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import axios from 'axios';
 import Area from '@components/common/Area';
 import Pagination from '@components/common/grid/Pagination';
 import { Checkbox } from '@components/common/form/fields/Checkbox';
+import { useAlertContext } from '@components/common/modal/Alert';
 import StatusRow from '@components/common/grid/rows/StatusRow';
 import BasicRow from '@components/common/grid/rows/BasicRow';
 import { Card } from '@components/admin/cms/Card';
@@ -13,6 +15,107 @@ import { Form } from '@components/common/form/Form';
 import { Field } from '@components/common/form/Field';
 import SortableHeader from '@components/common/grid/headers/Sortable';
 import Filter from '@components/common/list/Filter';
+
+function Actions({ admins = [], selectedIds = [] }) {
+  const { openAlert, closeAlert } = useAlertContext();
+
+  const updateAdmins = async (status) => {
+    const promises = admins
+      .filter((a) => selectedIds.includes(a.uuid))
+      .map((a) =>
+        axios.patch(a.updateApi, {
+          status
+        })
+      );
+    await Promise.all(promises);
+    // Refresh the page
+    window.location.reload();
+  };
+
+  const actions = [
+    {
+      name: 'Disable',
+      onAction: () => {
+        openAlert({
+          heading: `Disable ${selectedIds.length} admins`,
+          content: 'Are you sure?',
+          primaryAction: {
+            title: 'Cancel',
+            onAction: closeAlert,
+            variant: 'primary'
+          },
+          secondaryAction: {
+            title: 'Disable',
+            onAction: async () => {
+              await updateAdmins(0);
+            },
+            variant: 'critical',
+            isLoading: false
+          }
+        });
+      }
+    },
+    {
+      name: 'Enable',
+      onAction: () => {
+        openAlert({
+          heading: `Enable ${selectedIds.length} admins`,
+          content: 'Are you sure?',
+          primaryAction: {
+            title: 'Cancel',
+            onAction: closeAlert,
+            variant: 'primary'
+          },
+          secondaryAction: {
+            title: 'Enable',
+            onAction: async () => {
+              await updateAdmins(1);
+            },
+            variant: 'critical',
+            isLoading: false
+          }
+        });
+      }
+    }
+  ];
+
+  return (
+    <tr>
+      {selectedIds.length === 0 && null}
+      {selectedIds.length > 0 && (
+        <td style={{ borderTop: 0 }} colSpan="100">
+          <div className="inline-flex border border-divider rounded justify-items-start">
+            <a href="#" className="font-semibold pt-3 pb-3 pl-6 pr-6">
+              {selectedIds.length} selected
+            </a>
+            {actions.map((action) => (
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  action.onAction();
+                }}
+                className="font-semibold pt-3 pb-3 pl-6 pr-6 block border-l border-divider self-center"
+              >
+                <span>{action.name}</span>
+              </a>
+            ))}
+          </div>
+        </td>
+      )}
+    </tr>
+  );
+}
+
+Actions.propTypes = {
+  selectedIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  admins: PropTypes.arrayOf(
+    PropTypes.shape({
+      uuid: PropTypes.string.isRequired,
+      updateApi: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 export default function AdminGrid({
   adminUsers: { items: admins, total, currentFilters = [] }
@@ -195,11 +298,11 @@ export default function AdminGrid({
           </tr>
         </thead>
         <tbody>
-          {/* <Actions
-            customers={customers}
+          <Actions
+            admins={admins}
             selectedIds={selectedRows}
             setSelectedRows={setSelectedRows}
-          /> */}
+          />
           {admins.map((a) => (
             <tr key={a.adminUserId}>
               <td>
@@ -323,6 +426,7 @@ export const query = `
           value
           text
         }
+        updateApi
       }
       total
       currentFilters {
