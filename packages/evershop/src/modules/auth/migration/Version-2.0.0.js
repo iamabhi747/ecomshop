f313f1c95b15840e3ab191400b25ea0fb9ad2920
Javaboiiii <devrajlokhande1610@gmail.com>
2024-07-20 12:19:12 +0530

    intialising the database for super admin

--- packages/evershop/src/modules/auth/migration/Version-2.0.0.js
+++ packages/evershop/src/modules/auth/migration/Version-2.0.0.js
@@ -4,8 +4,8 @@ module.exports = exports = async (connection) => {
     await execute(
         connection,
         `ALTER TABLE "admin_user"
-        ADD IF NOT EXISTS "store_uuid" UUID NOT NULL,
-        ADD IF NOT EXISTS "phone" varchar NOT NULL DEFAULT '1234567890',
-        ADD CONSTRAINT "STORE_UUID" FOREIGN KEY ("store_uuid") REFERENCES "store_info" ("uuid");`
+        ADD IF NOT EXISTS "store_uuid" UUID ,
+        ADD IF NOT EXISTS "phone" varchar NOT NULL DEFAULT '1234567890';`
     );
+    // ADD CONSTRAINT "STORE_UUID" FOREIGN KEY ("store_uuid") REFERENCES "store_info" ("uuid");
 };
