6abffa2de9dc5b5c8961736878be35b5b80f126b
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-07-26 12:47:39 +0530

    added reference of store_uuid in createProduct

--- packages/evershop/src/modules/catalog/api/createProduct/createProduct[finish].js
+++ packages/evershop/src/modules/catalog/api/createProduct/createProduct[finish].js
@@ -2,6 +2,8 @@ const createProduct = require('../../services/product/createProduct');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = async (request, response, delegate) => {
+  const currentAdminUser = request.getCurrentUser();
+  request.body.store_uuid = currentAdminUser?.store_uuid;
   const result = await createProduct(request.body, {
     routeId: request.currentRoute.id
   });
