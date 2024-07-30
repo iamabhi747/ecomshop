cc8b94bf3151ac10c8a0c450026eb6301757cc5f
Atharva Kavade <atharvadk27@gmail.com>
2024-07-30 20:19:33 +0530

    color changes in main banner

--- packages/evershop/src/modules/catalog/migration/Version-2.0.0.js
+++ packages/evershop/src/modules/catalog/migration/Version-2.0.0.js
@@ -4,7 +4,7 @@ module.exports = exports = async (connection) => {
     await execute(
         connection,
         `ALTER TABLE "product"
-          ADD COLUMN IF NOT EXISTS "store_uuid" UUID NOT NULL DEFAULT '5305318d-b01e-48f5-b564-7dff294c9b53',
+          ADD COLUMN IF NOT EXISTS "store_uuid" UUID NOT NULL DEFAULT '97627b9a-46d2-4f7a-853e-e42b1c646e62',
           ADD CONSTRAINT "STORE_UUID" FOREIGN KEY ("store_uuid") REFERENCES "store_info" ("uuid")`
     );
 };
