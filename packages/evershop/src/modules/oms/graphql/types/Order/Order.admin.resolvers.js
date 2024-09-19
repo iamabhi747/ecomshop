3346eff27c4297c29b2446aaadf5bbf0bcebf096
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-09-19 13:09:52 +0530

    fixed order seperation: now super admin can see all orders in grid

--- packages/evershop/src/modules/oms/graphql/types/Order/Order.admin.resolvers.js
+++ packages/evershop/src/modules/oms/graphql/types/Order/Order.admin.resolvers.js
@@ -2,14 +2,27 @@ const { select } = require('@evershop/postgres-query-builder');
 const { buildUrl } = require('@evershop/evershop/src/lib/router/buildUrl');
 const { getOrdersBaseQuery } = require('../../../services/getOrdersBaseQuery');
 const { OrderCollection } = require('../../../services/OrderCollection');
+const { getConfig } = require('../../../../../lib/util/getConfig');
 
 module.exports = {
   Query: {
     orders: async (_, { filters = [] }, { user }) => {
       const query = getOrdersBaseQuery();
+      
+      // If user is super admin then show all orders
+      // If user is store admin then show only orders that belong to the store
+      let isSuperAdmin = false;
+      if (user) {
+        const admin_super_uuid = getConfig('admin_super_uuid', null);
+        if (admin_super_uuid && user.uuid === admin_super_uuid) {
+          isSuperAdmin = true;
+        }
+      }
 
-      // Only show orders that belong to the current admin's store
-      query.where('order.store_uuid', '=', user.store_uuid);
+      if (!isSuperAdmin) {
+        query.where('order.store_uuid', '=', user.store_uuid);
+      }
+
 
       const root = new OrderCollection(query);
       await root.init(filters);
