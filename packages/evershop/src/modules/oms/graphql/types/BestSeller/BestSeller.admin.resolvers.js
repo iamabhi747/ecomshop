88e7b976b8ee7487530a4ac9ab4fb5264d24e530
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-09-19 13:42:52 +0530

    Bestseller products seperation

--- packages/evershop/src/modules/oms/graphql/types/BestSeller/BestSeller.admin.resolvers.js
+++ packages/evershop/src/modules/oms/graphql/types/BestSeller/BestSeller.admin.resolvers.js
@@ -4,11 +4,24 @@ const {
   getProductsBaseQuery
 } = require('@evershop/evershop/src/modules/catalog/services/getProductsBaseQuery');
 const { sql } = require('@evershop/postgres-query-builder');
+const { getConfig } = require('../../../../../lib/util/getConfig');
 
 module.exports = {
   Query: {
-    bestSellers: async () => {
+    // eslint-disable-next-line no-unused-vars
+    bestSellers: async (args, _, { user }) => {
       const query = getProductsBaseQuery();
+
+      // If user is super admin then show bestseller from all products
+      // If user is store admin then show bestseller from only products that belong to the store
+      let isSuperAdmin = false;
+      if (user) {
+        const admin_super_uuid = getConfig('admin_super_uuid', null);
+        if (admin_super_uuid && user.uuid === admin_super_uuid) {
+          isSuperAdmin = true;
+        }
+      }
+
       query
         .leftJoin('order_item')
         .on('product.product_id', '=', 'order_item.product_id');
@@ -21,6 +34,11 @@ module.exports = {
         .select('SUM(order_item.qty)', 'soldQty')
         .select('SUM(order_item.product_id)', 'sum')
         .where('order_item_id', 'IS NOT NULL', null);
+      
+      if (!isSuperAdmin) {
+        query.andWhere('product.store_uuid', '=', user.store_uuid);
+      }
+
       query
         .groupBy(
           'order_item.product_id',
